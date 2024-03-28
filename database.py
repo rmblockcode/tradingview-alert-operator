c5e77428a2cc85feb52911359295cc0d2ab31cb5
Ramon Marquez <rmblockcode@gmail.com>
2024-03-27 21:10:35 -0400

    Testing deploy

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+print('DB: ', SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
